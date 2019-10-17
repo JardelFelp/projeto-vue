4d095c634613c1576530e3cce06a0de2962ed55d
Jardel Knirsch <jardel.knirsch@meta.com.br>
2019-10-17 17:32:51 -0300

    Initial Commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,51 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld :qtdItens="qtdItens" />
+
+    <!-- v-on:click ou @click -->
+    <button
+      v-on:click="adicionarAoCarrinho()"
+    >Adicionar item ao Carrinho</button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+
+// https://br.vuejs.org/v2/guide/events.html
 import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
   name: 'home',
+
   components: {
     HelloWorld,
   },
+
+  data() {
+    return {
+      itens: []
+    }
+  },
+
+  computed: {
+    qtdItens() {
+      return this.itens.length;
+    }
+  },
+
+  methods: {
+    adicionarAoCarrinho() {
+      this.itens.push({
+        name: 'Mouse',
+        valor: 500
+      });
+    },
+
+    removerItemCarrinho() {
+      this.itens.pop();
+    }
+  }
 };
 </script>
